30020c1a723e0d1a74bb58c27e4b4d2c86e6e96a
Joseph <josepholusegun18@gmail.com>
2022-12-23 21:26:27 -0800

    Delete Personal Files directory

--- Personal Files/personal.m
+++
@@ -1,2 +0,0 @@
-b=date
-[c,x]=weekday(date)
--- Personal Files/personal1.m
+++
@@ -1,7 +0,0 @@
-clc
-clear
-
-a=[1 2;3 4;5 6;7 8]
-a(:,2)='' % replace column 2 with empty set
-a(:,2)=7  % fill column 2 with 7
-a(3,:)=5  % fill row 3 with 5
