b14b23a8073a824a7482ba68d01994f1e8c9a93d
Joseph <josepholusegun18@gmail.com>
2022-12-23 21:46:12 -0800

    Delete Alarm Setting directory

--- Alarm Setting/Untitled2.m
+++
@@ -1,6 +0,0 @@
-  hafr = dsp.AudioFileReader('am.mp3');
-       hap = dsp.AudioPlayer('SampleRate',45000);
-       while ~isDone(hafr)
-           audio = step(hafr);
-           step(hap, audio);
-       end
